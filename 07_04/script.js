a42ec67b49f383e2e3d23b338509c0d8a97205fc
Brandon <brandvex@gmail.com>
2023-03-30 17:14:04 -0500

    yeah

--- 07_04/script.js
+++ 07_04/script.js
@@ -9,3 +9,23 @@
  */
 
 
+const stuff = ["Liz", "airCon", "Computer", "Guitar", "Bed", "TV", "Microphone", "Blanket"]
+console.log(stuff)
+
+stuff.pop()  //removes last item in array
+console.log(stuff)
+
+stuff.unshift(stuff.pop()) //moves the last item to the first position
+console.log(stuff)
+
+stuff.sort() //sorts the items alphabetically
+console.log(stuff)
+
+findString = () => {    //finds a specific item in the array
+    item = stuff[stuff.indexOf("Guitar")]
+    return item
+}
+console.log(stuff.find(findString))
+
+stuff.splice(stuff.indexOf("TV"), 1) //removes an item with specific content from the array
+console.log(stuff)
